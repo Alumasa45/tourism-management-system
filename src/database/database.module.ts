33f49b04ddb12716dcadadf89862016226f21585
Alumasa45 <aluquinn@gmail.com>
2025-06-03 14:41:50 +0300

    switched to pg4admin

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -17,10 +17,13 @@ import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
           const config: TypeOrmModuleOptions = {
             type: 'postgres',
             host: configService.get<string>('DB_HOST', 'localhost'),
-            port: configService.get<number>('DB_PORT', 5432),
+            port: configService.get<number>('DB_PORT', 5433),
             username: configService.get<string>('DB_USERNAME', 'postgres'),
-            password: configService.get<string>('DB_PASSWORD', 'postgres'),
-            database: configService.get<string>('DB_DATABASE', 'tourism_management'),
+            password: configService.get<string>('DB_PASSWORD', 'aquinattaayo'),
+            database: configService.get<string>(
+              'DB_DATABASE',
+              'tourism_management',
+            ),
             entities: [__dirname + '/../**/*.entity{.ts,.js}'],
             synchronize: configService.get<boolean>('DB_SYNC', true),
             logging: configService.get<boolean>('DB_LOGGING', false),
